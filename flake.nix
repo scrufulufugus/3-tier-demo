3402d23c1170fb19f96593c720c8f3af15ef2178
scrufulufugus <smonson@irbash.net>
2024-02-15 13:42:55 -0800

    Add support for frontend

--- flake.nix
+++ flake.nix
@@ -15,17 +15,26 @@
       let
         # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
         pkgs = nixpkgs.legacyPackages.${system};
+        middlePath = ./middleware;
+        frontPath = ./frontend;
         inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
       in
       {
         packages = {
-          middleware = mkPoetryApplication { projectDir = ./middleware; };
+          middleware = mkPoetryApplication { projectDir = middlePath; };
+          frontend = pkgs.mkYarnPackage {
+            name = "frontend";
+            src = frontPath;
+            packageJSON = "${frontPath}/package.json";
+            yarnLock = "${frontPath}/yarn.lock";
+            #yarnNix = "${frontPath}/yarn.nix";
+          };
           default = self.packages.${system}.middleware;
         };
 
         devShells.default = pkgs.mkShell {
-          inputsFrom = [ self.packages.${system}.middleware ];
-          packages = [ pkgs.poetry ];
+          inputsFrom = [ self.packages.${system}.frontend self.packages.${system}.middleware ];
+          packages = with pkgs; [ poetry yarn ];
         };
       });
 }
