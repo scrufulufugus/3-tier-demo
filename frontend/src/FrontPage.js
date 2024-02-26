4cc170e56882752db68d65fd89674c292f115745
scrufulufugus <smonson@irbash.net>
2024-02-26 15:07:43 -0800

    frontend: Add Page routes and header

---
+++ frontend/src/FrontPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header';
+
+function FrontPage() {
+  return (
+    <div className="FrontPage">
+      <Header/>
+      <h1>Front Page</h1>
+    </div>
+  );
+}
+
+export default FrontPage;
