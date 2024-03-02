9bdc0760987e3b56bdbffecfc89fbfffdfc768db
scrufulufugus <smonson@irbash.net>
2024-03-02 15:03:02 -0800

    Update account model to pull from backend

--- frontend/lib/models/account.dart
+++ frontend/lib/models/account.dart
@@ -1,77 +1,186 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class AccountModel extends ChangeNotifier {
   AccountModel();
 
-  Account? _account;
   String? _token;
 
   String get token => _token ?? 'tester:password123';
 
+  Future<AccountIn> get info async {
+    final response = await http.get(
+      Uri.parse('http://localhost:8000/user/me'),
+      headers: <String, String>{
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      return AccountIn.fromJson(jsonDecode(response.body));
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to get account');
+    }
+  }
+
   bool get isAuthenticated {
-    return _account != null;
+    return _token != null;
   }
 
+  // Future<bool> get isAdmin async {
+  //   return info.then((account) => account.isAdmin);
+  // }
+  // TODO: Find way to make this work
   bool get isAdmin {
-    return _account?.isAdmin ?? false;
+    return true;
   }
 
-  void login(String username, String password) {
-    // TODO: Make a request to the server to login
-    _account = Account(1, username, "test@example.com", password,
-        "1234567890", "1234 Elm St", true);
-    notifyListeners();
+  Future<bool> login(String username, String password) async {
+    final response = await http.post(
+      Uri.parse('http://localhost:8000/token'),
+      headers: <String, String>{
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      encoding: Encoding.getByName('utf-8'),
+      body: <String, String>{
+        'username': username,
+        'password': password,
+      },
+    );
+    if (response.statusCode == 200) {
+      _token = jsonDecode(response.body)['access_token'];
+      notifyListeners();
+      return true;
+    } else if (response.statusCode == 401) {
+      return false;
+    } else {
+      throw Exception('Failed to login');
+    }
   }
 
   void logout(BuildContext context) {
     context.go('/signout');
-    _account = null;
+    _token = null;
     notifyListeners();
   }
 
-  Account get info {
-    return Account(
-        _account?.id ?? 0,
-        _account?.username ?? '',
-        _account?.email ?? '',
-        '',
-        _account?.phone ?? '',
-        _account?.address ?? '',
-        false
-    );
-  }
 
-  String updateAccount(String? email, String? password,
-      String? phone, String? address, String currentPassword) {
-    if (_account == null) {
+  Future<String> update(AccountOut update, String currentPassword) async {
+    if (!isAuthenticated) {
       return "You must be logged in to update your account";
     }
+    if (!await login((await info).username, currentPassword)) {
+      return "Incorrect password";
+    }
 
-    Account updated = Account(
-        _account!.id,
-        _account!.username,
-        email ?? _account!.email,
-        password ?? _account!.password,
-        phone ?? _account!.phone,
-        address ?? _account!.address,
-        _account!.isAdmin);
-
-    // TODO: Make a request to the server to update the account
-    _account = updated;
-    notifyListeners();
-    return "Account updated";
+    final response = await http.patch(
+      Uri.parse('http://localhost:8000/user/me'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer $token',
+      },
+      body: jsonEncode(update),
+    );
+    if (response.statusCode == 200) {
+      notifyListeners();
+      return "Account updated";
+    } else {
+      return "Failed to update account";
+    }
   }
 }
 
 class Account {
-  Account(this.id, this.username, this.email, this.password, this.phone,
-      this.address, this.isAdmin);
-  final int id;
-  final String username;
-  final String email;
-  final String password;
-  final String phone;
-  final String address;
-  final bool isAdmin;
+  final int? id;
+  final String? username;
+  final String? email;
+  final String? password;
+  final String? phone;
+  final String? address;
+  final bool? isAdmin;
+
+  Account({
+      required this.id,
+      required this.username,
+      required this.email,
+      required this.password,
+      required this.phone,
+      required this.address,
+      required this.isAdmin
+  });
+
+  factory Account.fromJson(Map<String, dynamic> json) {
+    return Account(
+        id: json['id'],
+        username: json['username'],
+        email: json['email'],
+        password: json['password'],
+        phone: json['phone'],
+        address: json['address'],
+        isAdmin: json['isAdmin']
+    );
+  }
+}
+
+class AccountIn extends Account {
+  AccountIn({
+      required int super.id,
+      required String super.username,
+      required String super.email,
+      required String super.phone,
+      required String super.address,
+      required bool super.isAdmin
+  }) : super(
+      password: null,
+  );
+
+  @override
+  int get id => super.id!;
+  @override
+  String get username => super.username!;
+  @override
+  String get email => super.email!;
+  @override
+  String get phone => super.phone!;
+  @override
+  String get address => super.address!;
+  @override
+  bool get isAdmin => super.isAdmin!;
+
+  factory AccountIn.fromJson(Map<String, dynamic> json) {
+    return AccountIn(
+        id: json['id'],
+        username: json['username'],
+        email: json['email'],
+        phone: json['phone'],
+        address: json['address'],
+        isAdmin: json['isAdmin']
+    );
+  }
+}
+
+class AccountOut extends Account {
+  AccountOut({
+      required int id,
+      super.email,
+      super.password,
+      super.phone,
+      super.address,
+  }) : super(
+      id: id,
+      username: null,
+      isAdmin: null
+  );
+
+  static Map<String, dynamic> toJson(Account value) =>
+      {
+        'email': value.email,
+        'phone': value.phone,
+        'address': value.address,
+        'isAdmin': value.isAdmin
+      }..removeWhere((key, value) => value == null);
 }
--- frontend/lib/screens/account.dart
+++ frontend/lib/screens/account.dart
@@ -23,119 +23,141 @@ class _AccountState extends State<AccountPage> {
     return Scaffold(
       appBar: const Header(),
       body: Consumer<AccountModel>(
-        builder: (context, account, child) => Form(
-          key: _formKey,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
+        builder: (context, account, child) => FutureBuilder<AccountIn>(
+          future: account.info,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Form(
+                key: _formKey,
+                child: Padding(
                   padding:
                       const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                  child: Text(
-                    account.info.username,
-                    //style: Theme.of(context).primaryTextTheme.titleLarge,
-                  ),
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                  child: TextFormField(
-                    controller: emailController..text = account.info.email,
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(), labelText: "Email"),
-                    validator: (value) {
-                      return null;
-                    },
-                  ),
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                  child: TextFormField(
-                    controller: passwordController,
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(), labelText: "Password"),
-                    validator: (value) {
-                      return null;
-                    },
-                  ),
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                  child: TextFormField(
-                    controller: phoneController..text = account.info.phone,
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: "Phone Number"),
-                    validator: (value) {
-                      return null;
-                    },
-                  ),
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                  child: TextFormField(
-                    controller: addressController..text = account.info.address,
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(), labelText: "Address"),
-                    validator: (value) {
-                      return null;
-                    },
-                  ),
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                  child: TextFormField(
-                    controller: currentPasswordController,
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: "Current Password"),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your current password';
-                      }
-                      return null;
-                    },
-                  ),
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 8, vertical: 16.0),
-                  child: Center(
-                    child: ElevatedButton(
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          // Submit to backend
-                          String status = account.updateAccount(
-                              emailController.text,
-                              passwordController.text,
-                              phoneController.text,
-                              addressController.text,
-                              currentPasswordController.text);
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 8, vertical: 16),
+                        child: Text(
+                          snapshot.data!.username,
+                          //style: Theme.of(context).primaryTextTheme.titleLarge,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 8, vertical: 16),
+                        child: TextFormField(
+                          controller: emailController
+                            ..text = snapshot.data!.email,
+                          decoration: const InputDecoration(
+                              border: OutlineInputBorder(), labelText: "Email"),
+                          validator: (value) {
+                            return null;
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 8, vertical: 16),
+                        child: TextFormField(
+                          controller: passwordController,
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: "Password"),
+                          validator: (value) {
+                            return null;
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 8, vertical: 16),
+                        child: TextFormField(
+                          controller: phoneController
+                            ..text = snapshot.data!.phone,
+                          decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: "Phone Number"),
+                          validator: (value) {
+                            return null;
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 8, vertical: 16),
+                        child: TextFormField(
+                          controller: addressController
+                            ..text = snapshot.data!.address,
+                          decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: "Address"),
+                          validator: (value) {
+                            return null;
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 8, vertical: 16),
+                        child: TextFormField(
+                          controller: currentPasswordController,
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: "Current Password"),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your current password';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 8, vertical: 16.0),
+                        child: Center(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                // Submit to backend
+                                AccountOut update = AccountOut(
+                                    id: snapshot.data!.id,
+                                    email: emailController.text,
+                                    password: passwordController.text,
+                                    phone: phoneController.text,
+                                    address: addressController.text);
 
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(content: Text(status)),
-                          );
-                        } else {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(content: Text('Please fill input')),
-                          );
-                        }
-                      },
-                      child: const Text('Submit'),
-                    ),
+                                // TODO: Make async call to update account
+                                //String status = account.update(update, currentPasswordController.text);
+                                String status = "Success";
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(content: Text(status)),
+                                );
+                              } else {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                      content: Text('Please fill input')),
+                                );
+                              }
+                            },
+                            child: const Text('Submit'),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            ),
-          ),
+              );
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
+
+            // By default, show a loading spinner.
+            return const CircularProgressIndicator();
+          },
         ),
       ),
     );
