72d58a0ddd57a4b2f1a2635cdd5f73bfb75d66b3
scrufulufugus <smonson@irbash.net>
2024-03-02 16:30:24 -0800

    Add the option to build frontend for web

--- frontend/default.nix
+++ frontend/default.nix
@@ -4,6 +4,10 @@ builder rec {
   name = "frontend3Tier";
   pname = name;
   version = "1.0.0+1";
+
+  # To build for the Web, use the targetFlutterPlatform argument.
+  #targetFlutterPlatform = "web";
+
   src = ./.;
   autoPubspecLock = ./pubspec.lock;
 }
